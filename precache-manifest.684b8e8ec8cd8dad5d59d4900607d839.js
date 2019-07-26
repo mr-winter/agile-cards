8a91c41e461719ea803f8b3b92773d90a261240b
Thomas Winter <thomas.winter@fluxfederation.com>
2019-07-26 16:04:15 +1200

    Updates

---
+++ precache-manifest.684b8e8ec8cd8dad5d59d4900607d839.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aefc5c3f9c9b06072e238ef11b11a35d",
+    "url": "/mr-winter/agile-cards/index.html"
+  },
+  {
+    "revision": "7e207ec18601184e243c",
+    "url": "/mr-winter/agile-cards/static/js/2.6cad51d3.chunk.js"
+  },
+  {
+    "revision": "bd5adda3f5b9da2f5878",
+    "url": "/mr-winter/agile-cards/static/js/main.129d33b0.chunk.js"
+  },
+  {
+    "revision": "401ab5c76f59e1edcf67",
+    "url": "/mr-winter/agile-cards/static/js/runtime~main.e58987cf.js"
+  }
+]);
